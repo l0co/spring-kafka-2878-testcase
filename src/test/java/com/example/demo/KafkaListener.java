d0f7cf2ef34aae6ac7319db4535b3ef7d613d6ab
Lukasz Frankowski <lukasz.frankowski@zondaglobal.com>
2024-01-30 11:27:37 +0100

    spring kafka #2827 testcase

--- src/test/java/com/example/demo/KafkaListener.java
+++ src/test/java/com/example/demo/KafkaListener.java
@@ -16,10 +16,17 @@ public class KafkaListener {
     public static final Logger logger = LoggerFactory.getLogger(KafkaListener.class);
 
     public CountDownLatch latch = new CountDownLatch(1);
+    private int numberOfRetries = 0;
 
     @org.springframework.kafka.annotation.KafkaListener(topics = "topic")
     public void onEvent(@NonNull String content) {
-        logger.info("Received: {}", content);
+        logger.info("Received: {} [{}]", content, numberOfRetries);
+        if (numberOfRetries == 0) {
+            numberOfRetries++;
+            logger.info("Throwing...");
+            throw new RuntimeException("Expected");
+        }
+        logger.info("Continuing...");
         latch.countDown();
     }
 
